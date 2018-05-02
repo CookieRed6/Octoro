3d18f4f72bc5b551143c6fc0d1e6acd894184062
CookieRed6 <38787471+CookieRed6@users.noreply.github.com>
2018-05-02 10:48:29 -0500

    Create Octoro.bat

---
+++ Octoro.bat
@@ -0,0 +1,16 @@
+@echo off
+color 0a
+title Octoro
+cls
+:start
+cls
+echo  _____________________________________
+echo /              Octoro                 \
+echo [             Loading...              ]
+echo \_____________________________________/
+echo.
+sleep 10
+pause
+goto home
+
+:home
