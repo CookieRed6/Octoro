96ddf797e4ffe47ed6ba79a152194b1c963f9fdd
CookieRed6 <38787471+CookieRed6@users.noreply.github.com>
2018-05-03 10:34:43 -0500

    Update Octoro.bat

--- Octoro.bat
+++ Octoro.bat
@@ -31,6 +31,7 @@ SET /P INPUT=Please select an Option:
 IF /I '%INPUT%'=='1' GOTO 1
 IF /I '%INPUT%'=='2' GOTO 2
 IF /I '%INPUT%'=='3' GOTO 3
+IF /I '%INPUT%'=='4' GOTO TOP
 
 :1
 start www.github.com
@@ -46,3 +47,114 @@ echo  Version: 0.0.9
 echo  Made by CookieRed6
 pause
 goto home
+
+:TOP
+color 3f
+Cls
+ECHO ----------------------------------------------------
+ECHO[
+ECHO                    DOS CALCULATOR
+ECHO[
+ECHO ----------------------------------------------------
+ECHO[
+ECHO[
+ECHO    + = Sum
+ECHO[
+ECHO    - = Sub
+ECHO[
+ECHO    / = Division
+ECHO[
+ECHO    * = Multiply
+ECHO[
+ECHO    ~ = Exit
+ECHO[
+ECHO[
+ECHO ----------------------------------------------------
+ECHO[
+SET/P ch=" Enter Symbol "
+IF %ch% EQU + GOTO:SUM
+IF %ch% EQU - GOTO:SUB
+IF %ch% EQU / GOTO:DIV
+IF %ch% EQU * GOTO:MUL
+IF %ch% EQU ~ GOTO:EXI
+IF %ch% GTR . GOTO:Hel
+:SUM
+CLS
+ECHO ---------------------------------------------------
+ECHO[
+ECHO                         SUM
+ECHO[
+ECHO ---------------------------------------------------
+ECHO[
+set /p A=" Enter First Number = "
+ECHO[
+set /p B=" Enter Second Number = "
+SET /A C=A+B
+ECHO[
+ECHO ---------------------------------------------------
+ECHO %A%+%B%=%C%
+PAUSE
+GOTO:TOP
+:SUB
+CLS
+ECHO ---------------------------------------------------
+ECHO[
+ECHO                     SUBTRACTION
+ECHO[
+ECHO ---------------------------------------------------
+ECHO[
+set /p A=" Enter First Number = "
+ECHO[
+set /p B=" Enter Second Number = "
+SET /A C=A-B
+ECHO[
+ECHO ---------------------------------------------------
+ECHO %A%-%B%=%C%
+PAUSE
+GOTO:TOP
+:DIV
+CLS
+ECHO ----------------------------------------------------
+ECHO[
+ECHO                      DIVISION
+ECHO[
+ECHO -----------------------------------------------------
+ECHO[
+set /p A=" Enter First Number = "
+ECHO[
+set /p B=" Enter Second Number = "
+SET /A C=A/B
+ECHO[
+ECHO -----------------------------------------------------
+ECHO %A%/%B%=%C%
+PAUSE
+GOTO:TOP
+:MUL
+CLS
+ECHO ---------------------------------------------------
+ECHO[
+ECHO                    MULTIPICATION
+ECHO[
+ECHO ----------------------------------------------------
+ECHO[
+set /p A=" Enter First Number = "
+ECHO[
+set /p B=" Enter Second Number = "
+SET /A C=A*B
+ECHO[
+ECHO ----------------------------------------------------
+ECHO %A%*%B%=%C%
+PAUSE
+GOTO:TOP
+:EXI
+goto home
+:HEL
+CLS
+ECHO --------------------------------------------------
+ECHO[
+ECHO                         HELP
+ECHO[
+ECHO --------------------------------------------------
+ECHO Press (+) For Sum, (-) For Sub, (/) for Division, (*) for Multiply.
+PAUSE
+GOTO:TOP
